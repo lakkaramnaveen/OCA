28d9bf920efe8bb7babfbcffada90403f335e0a1
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-09 17:40:10 -0400

    duplicate characters using three different methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,51 @@
 package org.example;
 
+import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(findDuplicatesUsingFor("code decode"));
+        System.out.println(findDuplicatesUsingArrays("code decode"));
+        System.out.println(findDuplicatesUsingMaps("code decode"));
+    }
+
+    private static Set<Character> findDuplicatesUsingMaps(String codeDecode) {
+        Set<Character> duplicates = new LinkedHashSet<>();
+        HashMap<Character, Integer> countMap = new HashMap<Character, Integer>();
+        for (int i = 0; i < codeDecode.length(); i++){
+            if(countMap.containsKey(codeDecode.charAt(i))){
+                countMap.put(codeDecode.charAt(i), countMap.get(codeDecode.charAt(i)) + 1);
+            }else{
+                countMap.put(codeDecode.charAt(i), 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry: countMap.entrySet()) {
+            if (entry.getValue()>1){
+                duplicates.add(entry.getKey());
+            }
+        }
+        return duplicates;
+    }
+
+    private static Set<Character> findDuplicatesUsingArrays(String name){
+        Set<Character> duplicates = new LinkedHashSet<>();
+        int[] arrayForChar = new int[256];
+
+        for(int i=0;i<name.length();i++){
+            arrayForChar[name.charAt(i)] = arrayForChar[name.charAt(i)] + 1;
+        }
+
+        for(int i =0;i<256;i++){
+            if(arrayForChar[i]>1){
+                duplicates.add((char) i);
+            }
+        }
+
+        return duplicates;
     }
 
     private static Set<Character> findDuplicatesUsingFor(String codeDecode) {
