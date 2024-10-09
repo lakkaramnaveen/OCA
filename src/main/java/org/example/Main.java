6af87691cafb973d0f6d57ad9c5fab6e7a37043f
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-09 18:46:14 -0400

    string constant pool

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = "code";
-        String s3 = "code";
-        System.out.println(s1 == s3);
+        String s1 = new String("code");
+        String s2 = "code";
+        String s3 = s2;
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s2==s3);
 
-        String s2 = new String("code");
-        String s4 = new String("code");
-        System.out.println(s2 == s4);
     }
 }
