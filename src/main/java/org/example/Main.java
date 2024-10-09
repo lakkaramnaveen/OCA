07b021de18be0949777e0d3dc9ca502a13b46213
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-09 18:31:51 -0400

    string constant pool

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,58 +7,12 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(findDuplicatesUsingFor("code decode"));
-        System.out.println(findDuplicatesUsingArrays("code decode"));
-        System.out.println(findDuplicatesUsingMaps("code decode"));
-    }
+        String s1 = "code";
+        String s3 = "code";
+        System.out.println(s1 == s3);
 
-    private static Set<Character> findDuplicatesUsingMaps(String codeDecode) {
-        Set<Character> duplicates = new LinkedHashSet<>();
-        HashMap<Character, Integer> countMap = new HashMap<Character, Integer>();
-        for (int i = 0; i < codeDecode.length(); i++){
-            if(countMap.containsKey(codeDecode.charAt(i))){
-                countMap.put(codeDecode.charAt(i), countMap.get(codeDecode.charAt(i)) + 1);
-            }else{
-                countMap.put(codeDecode.charAt(i), 1);
-            }
-        }
-
-        for (Map.Entry<Character, Integer> entry: countMap.entrySet()) {
-            if (entry.getValue()>1){
-                duplicates.add(entry.getKey());
-            }
-        }
-        return duplicates;
-    }
-
-    private static Set<Character> findDuplicatesUsingArrays(String name){
-        Set<Character> duplicates = new LinkedHashSet<>();
-        int[] arrayForChar = new int[256];
-
-        for(int i=0;i<name.length();i++){
-            arrayForChar[name.charAt(i)] = arrayForChar[name.charAt(i)] + 1;
-        }
-
-        for(int i =0;i<256;i++){
-            if(arrayForChar[i]>1){
-                duplicates.add((char) i);
-            }
-        }
-
-        return duplicates;
-    }
-
-    private static Set<Character> findDuplicatesUsingFor(String codeDecode) {
-        Set<Character> duplicates = new LinkedHashSet<>();
-
-        for(int i=0; i<codeDecode.length();i++){
-            for(int j=i+1; j<codeDecode.length();j++) {
-                if(codeDecode.charAt(i)==codeDecode.charAt(j)){
-                    duplicates.add(codeDecode.charAt(j));
-                }
-            }
-        }
-
-        return duplicates;
+        String s2 = new String("code");
+        String s4 = new String("code");
+        System.out.println(s2 == s4);
     }
 }
