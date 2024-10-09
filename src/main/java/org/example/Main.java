a7de0b42a83d76645de21e55e68eba0e4f6d74a2
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-09 17:07:58 -0400

    Substring updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String originalString = "decode";
-        String toBeChecked = "decode";
-        int rotateCharacters = 6;
-        System.out.println(checkForRotation(originalString, toBeChecked));
-        System.out.println(leftRotate(originalString, rotateCharacters));
-        System.out.println(rightRotate(originalString, rotateCharacters));
-    }
+        String originalString = "code";
+        String subString = originalString.substring(1,3);
+        originalString = null;
 
-    private static String rightRotate(String originalString, int r) {
-        int partition = originalString.length() - r;
-        return originalString.substring(partition) + originalString.substring(0,partition);
-    }
+        System.out.println(subString);
 
-    private static String leftRotate(String originalString, int r) {
-        return originalString.substring(r) + originalString.substring(0,r);
-    }
-
-    private static boolean checkForRotation(String originalString, String toBeChecked) {
-        String concatenatedString = originalString + originalString;
-        return concatenatedString.contains(toBeChecked);
     }
 }
