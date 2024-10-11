d68ba09d49d7b99ac664a1af4f8365e8977e007b
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-11 19:04:51 -0400

    Sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        String s1 = new String("code");
-        String s2 = "code";
-        String s3 = s2;
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
-        System.out.println(s2==s3);
+        List<Employee> employees = new ArrayList<Employee>();
+        employees.add(new Employee(2,2000));
+        employees.add(new Employee(1,3000));
+        employees.add(new Employee(3,4000));
+        employees.add(new Employee(4,5000));
+        employees.add(new Employee(5,1000));
+        employees.add(new Employee(6,1200));
+        employees.add(new Employee(7,2200));
 
+        List<Employee> em = employees.stream().sorted((e1, e2)-> (int) (e2.getSalary()-e1.getSalary()))
+                .skip(3)
+                .collect(Collectors.toList());
+        System.out.println(em);
+
+        //        for (Employee emp: em){
+//            System.out.println(emp.getSalary());
+//        }
     }
 }
