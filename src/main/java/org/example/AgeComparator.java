a152591b81306c790dc65438e709e9f860f69453
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-12 17:45:09 -0400

    Different Comparators

--- src/main/java/org/example/AgeComparator.java
+++ src/main/java/org/example/AgeComparator.java
@@ -7,6 +7,6 @@ public class AgeComparator implements Comparator<Employee> {
 
     @Override
     public int compare(Employee o1, Employee o2) {
-        return o1.getAge() - o2.getAge();
+        return o2.getAge() - o1.getAge();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,6 +23,10 @@ public class Main {
         employeeList.add(e5);
 
 
-        System.out.println(employeeList);
+        System.out.println(" Unsorted List "+employeeList);
+
+
+        Collections.sort(employeeList, new AgeComparator());
+        System.out.println(" Sorted base on age "+employeeList);
     }
 }
