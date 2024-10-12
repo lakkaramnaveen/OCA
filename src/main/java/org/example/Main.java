ef916d1dc4a96181ff823e4cb1872bd8ed2809dd
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-12 14:46:16 -0400

    Array vs ArrayList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -7,22 +8,12 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Employee> employees = new ArrayList<Employee>();
-        employees.add(new Employee(2,2000));
-        employees.add(new Employee(1,3000));
-        employees.add(new Employee(3,4000));
-        employees.add(new Employee(4,5000));
-        employees.add(new Employee(5,1000));
-        employees.add(new Employee(6,1200));
-        employees.add(new Employee(7,2200));
+        int[] a = new int[10];
+        int s = a.length;
+        System.out.println(s);
+        ArrayList<Integer> at = new ArrayList<Integer>();
 
-        List<Employee> em = employees.stream().sorted((e1, e2)-> (int) (e2.getSalary()-e1.getSalary()))
-                .skip(3)
-                .collect(Collectors.toList());
-        System.out.println(em);
+        Integer[] integer = new Integer[10];
 
-        //        for (Employee emp: em){
-//            System.out.println(emp.getSalary());
-//        }
     }
 }
