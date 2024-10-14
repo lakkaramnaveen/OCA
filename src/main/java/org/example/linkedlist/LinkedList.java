2f24ff035d8307b153c2f2844d745ab9977ddc15
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-14 18:36:06 -0400

    Linked lists

---
+++ src/main/java/org/example/linkedlist/LinkedList.java
@@ -0,0 +1,15 @@
+package org.example.linkedlist;
+
+public class LinkedList {
+    class Node{
+        int data;
+        Node next = null;
+
+        Node(int data){
+            this.data = data;
+        }
+
+        Node head, tail = null;
+
+    }
+}
