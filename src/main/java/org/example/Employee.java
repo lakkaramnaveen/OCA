2a1acdf930b1588ec73a4ea491d0b5cbea55fbb9
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-14 09:18:44 -0400

    Different Comparators

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -5,53 +5,7 @@ public class Employee {
     private int age;
     private String name;
     private Address address;
+    private int salary;
 
-    public Employee(int id, int age, String name, Address address) {
-        this.id = id;
-        this.age = age;
-        this.name = name;
-        this.address = address;
-    }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", age=" + age +
-                ", name='" + name + '\'' +
-                ", address=" + address +
-                '}';
-    }
 }
