2e871aab36ae5328519649be4486f209f993d5d4
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-29 10:36:01 -0400

    Func

--- src/main/java/org/example/java/FunctionalDemo.java
+++ src/main/java/org/example/java/FunctionalDemo.java
@@ -5,10 +5,9 @@ public interface FunctionalDemo {
     abstract void sum(int a, int b);
 
     static void sum2(int a, int b) {
-
+        System.out.println(a+b);
     }
 
     default void sum3(int a, int b) {
-
     }
 }
