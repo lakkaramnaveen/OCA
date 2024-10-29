16dc1c295d273e940e63c323192cac3fd44b58de
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-29 14:17:00 -0400

    main method update to "void main" from  'public static void main'

--- src/main/java/org/example/java/Lambda.java
+++ src/main/java/org/example/java/Lambda.java
@@ -5,8 +5,6 @@ import java.util.function.BiConsumer;
 public class Lambda {
     void main(String[] args) {
         System.out.println(addTwoNumbers(2,3));
-
-
         BiConsumer<Integer, Integer> biConsumer = (a,b) -> System.out.println(a+b);
         biConsumer.accept(10,5);
     }
