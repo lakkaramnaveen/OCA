a93d3d5bbb9ac9275e6a41b5065b525b0a9f3962
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-29 14:16:46 -0400

    main method update to "void main" from  'public static void main'

--- src/main/java/org/example/java/Demo.java
+++ src/main/java/org/example/java/Demo.java
@@ -7,7 +7,7 @@ interface A {
 
 
 public class Demo {
-    public static void main(String[] args) {
-
+    void main(String[] args){
+        System.out.println("Nani");
     }
 }
--- src/main/java/org/example/java/Lambda.java
+++ src/main/java/org/example/java/Lambda.java
@@ -3,7 +3,7 @@ package org.example.java;
 import java.util.function.BiConsumer;
 
 public class Lambda {
-    public static void main(String[] args) {
+    void main(String[] args) {
         System.out.println(addTwoNumbers(2,3));
 
 
