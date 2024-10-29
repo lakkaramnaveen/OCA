143efc1927478bf457ddb45eababd564e9ff5150
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-29 06:41:27 -0400

    Lambda

---
+++ src/main/java/org/example/java/Lambda.java
@@ -0,0 +1,17 @@
+package org.example.java;
+
+import java.util.function.BiConsumer;
+
+public class Lambda {
+    public static void main(String[] args) {
+        System.out.println(addTwoNumbers(2,3));
+
+
+        BiConsumer<Integer, Integer> biConsumer = (a,b) -> System.out.println(a+b);
+        biConsumer.accept(10,5);
+    }
+
+    static int addTwoNumbers(int a, int b){
+        return  a+b;
+    }
+}
