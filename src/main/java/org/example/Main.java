df756d41c1f9bc56faf2a522211d5afe48b0d054
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-30 11:29:35 -0400

    Square Function and Cube Function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,21 +8,13 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-//        Predicate<String> p = s1 -> s1.length() > 5;
-//        System.out.println(p.test("code dfecode"));
-//
-//        Predicate<String> p1 = s1 -> s1.length() % 2 == 0;
-//        System.out.println(p1.test("cod"));
-//
-//        // Predicate Joining
-//        System.out.println(p.and(p1).test("toyota"));
-//
-
         // Function
         // Square function
         Function<Integer, Integer> f = i -> i * i;
         System.out.println(f.apply(2));
 
-
+        // Cube function
+        Function<Integer, Integer> f1 = i -> i * i * i;
+        System.out.println(f1.apply(2));
     }
 }
