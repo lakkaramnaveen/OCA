4f3c25412b58a49f304889acc87bc7c56b33cc2a
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-30 10:58:26 -0400

    Functional interface example -> Predicate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,26 +7,26 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee(1, 29, "nani", new Address("Street1", 456), 12000);
-        Employee e2 = new Employee(2, 22, "chini", new Address("Stree2", 232), 12222);
-        Employee e3 = new Employee(3, 21, "potti", new Address("Mam", 324), 12121);
-        Employee e4 = new Employee(4, 20, "min", new Address("Street4", 333), 10000);
-        Employee e5 = new Employee(5, 21, "chin", new Address("Street3", 334), 11000);
-
-        List<Employee> employeeList = new ArrayList<>();
-        employeeList.add(e1);
-        employeeList.add(e2);
-        employeeList.add(e3);
-        employeeList.add(e4);
-        employeeList.add(e5);
-
-        System.out.println(" Unsorted List "+employeeList);
-
-        Collections.sort(employeeList, new AgeComparator());
-        System.out.println(" Sorted list based on age "+employeeList);
-        String[] s = {"1","2","3","4"};
-        asa(s);
-        as(s);
+//        Employee e1 = new Employee(1, 29, "nani", new Address("Street1", 456), 12000);
+//        Employee e2 = new Employee(2, 22, "chini", new Address("Stree2", 232), 12222);
+//        Employee e3 = new Employee(3, 21, "potti", new Address("Mam", 324), 12121);
+//        Employee e4 = new Employee(4, 20, "min", new Address("Street4", 333), 10000);
+//        Employee e5 = new Employee(5, 21, "chin", new Address("Street3", 334), 11000);
+//
+//        List<Employee> employeeList = new ArrayList<>();
+//        employeeList.add(e1);
+//        employeeList.add(e2);
+//        employeeList.add(e3);
+//        employeeList.add(e4);
+//        employeeList.add(e5);
+//
+//        System.out.println(" Unsorted List "+employeeList);
+//
+//        Collections.sort(employeeList, new AgeComparator());
+//        System.out.println(" Sorted list based on age "+employeeList);
+//        String[] s = {"1","2","3","4"};
+//        asa(s);
+//        as(s);
 
         Predicate<String> p = s1 -> s1.length() > 5;
         System.out.println(p.test("code dfecode"));
