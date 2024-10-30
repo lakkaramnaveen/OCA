cc3c15f16ce6882e6cb3a3b74fe2d16e1bf6ad05
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-10-30 11:01:09 -0400

    Joining predicate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,11 @@ public class Main {
     public static void main(String[] args) {
         Predicate<String> p = s1 -> s1.length() > 5;
         System.out.println(p.test("code dfecode"));
+
+        Predicate<String> p1 = s1 -> s1.length() % 2 == 0;
+        System.out.println(p1.test("cod"));
+
+
+        System.out.println(p.and(p1).test("nani1"));
     }
 }
