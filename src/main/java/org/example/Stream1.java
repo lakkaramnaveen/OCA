3912cf4f6e08dfa694a504088309cbfd5ea1f01d
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-01 09:16:57 -0400

    Streams -> Processing

--- src/main/java/org/example/Stream1.java
+++ src/main/java/org/example/Stream1.java
@@ -2,20 +2,29 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class Stream {
+public class Stream1 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        list.add(36);
         list.add(12);
         list.add(34);
         list.add(21);
-
+        // Stream filters even
         List<Integer> l1 = list.stream().filter(n->n%2==0).toList();
+        // Count
         Long count = list.stream().filter(n->n%2==0).count();
+        // Sort
+        Stream<Integer> s1 = list.stream().filter(n->n%2==0).sorted();
+
         for(Integer i1: l1) {
             System.out.println(i1);
         }
         System.out.println(count);
+
+        for(Integer i1: s1) {
+            System.out.println(i1);
+        }
     }
 }
