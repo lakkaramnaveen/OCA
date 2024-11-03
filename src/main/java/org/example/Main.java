bd2b603c44cfceef583c9f9bc8d24749644d64c4
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-03 10:20:00 -0500

    Multi threading -> main thread

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,8 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
 public class Main {
     public static void main(String[] args) {
-        // Function
-        // Square function
-        Function<Integer, Integer> f = i -> i * i;
-        System.out.println(f.apply(2));
-
-        // Cube function
-        Function<Integer, Integer> f1 = i -> i * i * i;
-        System.out.println(f1.apply(2));
-
-        String name = "Shre /* ya */ Paul";
-        System.out.println(name);
+        System.out.println("Hello guys");
+        System.out.println(Thread.currentThread());
     }
 }
