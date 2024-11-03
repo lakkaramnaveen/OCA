0f721732a0e2ceb49426639e44393f71c5adbd65
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-03 14:51:20 -0500

    daemon thread

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         ThreadClass threadClass = new ThreadClass();
-        threadClass.setDaemon(false);
+        threadClass.setDaemon(true);
         threadClass.start();
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("In main thread");
         }
     }
--- src/main/java/org/example/ThreadClass.java
+++ src/main/java/org/example/ThreadClass.java
@@ -3,7 +3,7 @@ package org.example;
 public class ThreadClass extends Thread{
     @Override
     public void run() {
-        for (int i = 0; i < 10000 ; i++) {
+        for (int i = 0; i < 100 ; i++) {
             System.out.println("I am executed by a child thread");
             System.out.println("am a daemon " + Thread.currentThread().isDaemon());
         }
