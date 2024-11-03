6d5b4bb56e21c1a1d10f8ba040627428ea40c657
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-03 12:03:12 -0500

    access modifiers and how to use them

--- src/main/java/org/example/RunnableMain.java
+++ src/main/java/org/example/RunnableMain.java
@@ -1,9 +1,12 @@
 package org.example;
 
 public class RunnableMain {
+    private int n1;
     public static void main(String[] args) {
         RunnableDemo runnableDemo = new RunnableDemo();
         Thread thread = new Thread(runnableDemo);
         thread.start();
+        int n;
+        int k;
     }
 }
