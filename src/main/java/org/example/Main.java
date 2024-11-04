3aa13556fcb938417fb51a965b950ecce4c976ab
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-04 10:23:07 -0500

    exception

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,29 @@
 package org.example;
 
+import javax.lang.model.type.ArrayType;
+
 public class Main {
     public static void main(String[] args) {
         ThreadClass threadClass = new ThreadClass();
         threadClass.setDaemon(true);
         threadClass.start();
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("In main thread");
+        try{
+            for (int i = 0; i < 10; i++) {
+                System.out.println("In main thread");
+            }
+            int i =0;
+            i = 10/i;
+        } catch (NullPointerException e){
+            System.err.println("Nani is not fine");
+        } catch (StackOverflowError e){
+            System.err.println("Nani is not fine");
         }
+        catch (ArithmeticException e){
+            System.err.println("Nani is not fine, arthemetics");
+        }
+        catch (Exception e){
+            System.err.println("Nani is not fine, exception");
+        }
+
     }
 }
