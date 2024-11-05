2d60685a1db5c51c43ac0ef8d3f29512c2f5ec74
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-05 17:13:54 -0500

    counter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,10 @@
 package org.example;
 
-import javax.lang.model.type.ArrayType;
-
 public class Main {
     public static void main(String[] args) {
-        ThreadClass threadClass = new ThreadClass();
-        threadClass.setDaemon(true);
-        threadClass.start();
-        try{
-            for (int i = 0; i < 10; i++) {
-                System.out.println("In main thread");
-            }
-            int i =0;
-            i = 10/i;
-        } catch (NullPointerException e){
-            System.err.println("Nani is not fine");
-        } catch (StackOverflowError e){
-            System.err.println("Nani is not fine");
+        int counter = 0;
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
         }
-        catch (ArithmeticException e){
-            System.err.println("Nani is not fine, arthemetics");
-        }
-        catch (Exception e){
-            System.err.println("Nani is not fine, exception");
-        }
-
     }
 }
