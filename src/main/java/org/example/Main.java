76bafc9333989a0d1704d7ed5ef8fefd16e332e4
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-05 17:15:17 -0500

    none

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int counter = 0;
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i);
-            counter++;
-        }
-        System.out.println(counter);
+
     }
 }
