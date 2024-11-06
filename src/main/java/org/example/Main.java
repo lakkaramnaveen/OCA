22487166ea36d8f770d3722fe848aa4dffa71d1b
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-06 11:54:07 -0500

    year

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LocalDateTime.now().getDayOfYear());
+        System.out.println(LocalDateTime.now().getYear());
     }
 }
