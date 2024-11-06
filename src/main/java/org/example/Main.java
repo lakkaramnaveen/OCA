db119aeefc6fd1411e32481c33708c49032f1251
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-06 12:54:14 -0500

    sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now().getYear());
         System.out.println(LocalDateTime.now().getNano());
+        int[] ar = {1,3,232,242,12,121};
+
+        for (int i = 0; i < ar.length - 1; i++) {
+            if (ar[i]<ar[i+1]){
+                continue;
+            } else {
+                int temp = ar[i];
+                ar[i] = ar[i+1];
+                ar[i+1] = temp;
+            }
+        }
+
+        for (int a : ar){
+            System.out.println(a);
+        }
     }
 }
