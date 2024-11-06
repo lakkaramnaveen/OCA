57d8953a1bd509e8e1ee4c6552506cff99815bfc
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-06 11:54:36 -0500

    nano

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now().getYear());
+        System.out.println(LocalDateTime.now().getNano());
     }
 }
