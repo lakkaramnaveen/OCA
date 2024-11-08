7eed4e22275a4f18f68a62c16803ccd7b2cf42dd
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 09:59:09 -0500

    static variable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,6 @@ public class Main {
             Emp emp1 = new Emp();
             Emp emp2 = new Emp();
             emp1.bankVault = 10;
-            emp2.bankVault = 20;
-            System.out.println(emp1.bankVault);
-            System.out.println(emp2.bankVault);
-            System.out.println(Emp.bankVault);
+            System.out.println(Emp.getBankVault());
         }
 }
