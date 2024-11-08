d4dc5a4e19b2ef53e747317e93f83cf25773a890
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 10:22:25 -0500

    static variable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,14 @@ package org.example;
 
 public class Main {
         public static void main(String[] args) {
-            Emp emp1 = new Emp();
-            Emp emp2 = new Emp();
-            emp1.bankVault = 10;
-            System.out.println(Emp.getBankVault());
-            System.out.println(Emp.interest(1,2,3));
+            int x = 10;
+            int y = ++x;
+            int z = 0;
+            if (y>=10 | y<=++x){
+                z = x;
+            } else {
+                z = x++;
+            }
+            System.out.println(z);
         }
 }
