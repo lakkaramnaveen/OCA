840afbe92ab2d129f438b7e8cc8b74cbcd71d6ed
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 13:46:53 -0500

    1 12 123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print("* ");
+                for (int j = 1; j < i+2; j++) {
+                    System.out.print(j);
                 }
                 System.out.println();
             }
