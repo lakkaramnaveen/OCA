fd5fee384b8e265e80ee0e12a04a51abb070a78f
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 18:38:06 -0500

    A A B A B C A B C D A B C D E A B C D E F A B C D E F G A B C D E F G H A B C D E F G H I A B C D E F G H I J A B C D E F G H I J K A B C D E F G H I J K L

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -91,10 +91,9 @@ public class Main {
             }
         }
         static void print14(int n){
-            char c = 'A';
-            for (int i = 1; i <= n; i++) {
-                for (int j = 0; j < i; j++) {
-                    System.out.print(((char)(c+j))+" ");
+            for (int i = 0; i < n; i++) {
+                for (char j = 'A'; j <= 'A'+i ; j++) {
+                    System.out.print(j+" ");
                 }
                 System.out.println();
             }
