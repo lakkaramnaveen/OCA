d7b34e6cd96cfa92415fff1986edd59a00712cb8
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 17:04:23 -0500

    3 1 01 101

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -54,7 +54,7 @@ public class Main {
                 if (i%2==0) start =1;
                 else start = 0;
                 for (int j = 0; j <= i ; j++) {
-                    System.out.print(start);
+                    System.out.print(start+" ");
                     start = 1 - start;
                 }
                 System.out.println();
