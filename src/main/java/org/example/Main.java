42021f31b3618cf8d130d5eeccbc3a878bc67fd2
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 08:04:38 -0500

    compilation error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,16 @@
 package org.example;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        // Sorting
-        int[] ar = {1,23,45,12,3,5,2,7,24, 3, 4232, 32};
-        int n = ar.length;
-        for (int i = n-1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if(ar[j]>ar[j+1]){
-                    int temp = ar[j];
-                    ar[j]=ar[j+1];
-                    ar[j+1]= temp;
-                }
+        private static final List<Integer> list = new ArrayList<>();
+
+        public static void main(String[] args) {
+            for (int i = 0; i < 1000; i++) {
+                new Thread(() -> list.add(i)).start();
             }
         }
-        for (int i: ar){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        int[] ar1 = {1,23,45,12,3,5,2,7};
-        Arrays.stream(ar1).sorted().forEach(i-> System.out.print(i+" "));
-    }
 }
