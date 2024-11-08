2458d379f3d4fc955f24754fcf3b005dbf3b0d69
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 10:41:11 -0500

    ****

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,10 @@ package org.example;
 
 public class Main {
         public static void main(String[] args) {
-            int x = 0;
+            int x = 3;
             do {
                 System.out.print("*");
-            } while (x++ < 3);
+                x--;
+            } while (x>=0);
         }
 }
