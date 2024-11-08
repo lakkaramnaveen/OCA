0ebc0166ea34bacb1350c8d8dfa3106303f570b6
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 16:15:04 -0500

    3 1 01 101

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,9 @@ public class Main {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
+            print10(n);
+        }
+        static void print7(int n){
             for (int i = 0 ; i < n; i++ ) {
                 for (int j = 0; j < n-i-1; j++) {
                     System.out.print(" ");
@@ -18,6 +21,9 @@ public class Main {
                 }
                 System.out.println();
             }
+        }
+        static void print8(int n){
+
             for (int i = 0 ; i < n; i++ ) {
                 for (int j = 0; j < i; j++) {
                     System.out.print(" ");
@@ -27,6 +33,29 @@ public class Main {
                 }
                 for (int j = 0; j < i; j++) {
                     System.out.print(" ");
+                }
+                System.out.println();
+            }
+
+        }
+        static void print9(int n){
+            for (int i = 1; i <= 2*n-1 ; i++) {
+                int stars = i;
+                if (i>n) stars = 2*n-i;
+                for (int j = 1; j <= stars; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        static void print10(int n){
+            int start = 1;
+            for (int i = 0; i < n; i++) {
+                if (i%2==0) start =1;
+                else start = 0;
+                for (int j = 0; j <= i ; j++) {
+                    System.out.print(start);
+                    start = 1 - start;
                 }
                 System.out.println();
             }
