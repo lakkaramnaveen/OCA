15fc19996f073d17fa80f285f0160c2b100034fe
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-08 13:54:44 -0500

    3 321 32 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
             int n = sc.nextInt();
             for (int i = 0; i < n; i++) {
                 for (int j = n; j > i; j--) {
-                    System.out.print("* ");
+                    System.out.print(j);
                 }
                 System.out.println();
             }
