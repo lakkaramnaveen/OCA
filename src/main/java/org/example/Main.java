44fdeecaf5b618cf2689eb5289085ef20065e464
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-09 18:41:35 -0500

    streams

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,11 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         // StriverPatterns.print19(5);
         List<Integer> li = Arrays.asList(1,12,3,4,5,6,7);
-        li.stream().filter(n-> n%2!=0).map(s->s*s).sorted().forEach(s-> System.out.println(s));
+        System.out.println(li.parallelStream().filter(n -> n % 2 == 0).map(s -> s * s).reduce(0, (c, e) -> c + e));
     }
 }
