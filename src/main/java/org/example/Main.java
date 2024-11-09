179f6a0d74c249f4cb08daaadd9b8a3b474305b3
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-09 15:06:32 -0500

    streams

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         // StriverPatterns.print19(5);
-        List<Integer> li = Arrays.asList(1,223,21,121,31);
-        Stream<Integer> myDataStream = li.stream();
-        Stream<Integer> newStream = myDataStream.sorted();
-        newStream.forEach(s-> System.out.println(s));
+        List<Integer> li = Arrays.asList(1,12,3,4,5,6,7);
+        li.stream().filter(n-> n%2!=0).map(s->s*s).sorted().forEach(s-> System.out.println(s));
     }
 }
