08ee7bab3afd45a5a534816be88d52f2e85253ba
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-09 11:51:23 -0500

    A    ABA   ABCBA  ABCDCBA ABCDEDCBA

--- src/main/java/org/example/StriverPatterns.java
+++ src/main/java/org/example/StriverPatterns.java
@@ -116,8 +116,14 @@ public class StriverPatterns {
             }
 
             char ch = 'A';
+            int breakPoint = (2*i-1)/2;
             for (int j = 0; j < 2*i+1; j++) {
                 System.out.print(ch);
+                if (j<=breakPoint){
+                    ch++;
+                }else{
+                    ch--;
+                }
             }
 
             for (int j = 0; j < n-i-1; j++) {
