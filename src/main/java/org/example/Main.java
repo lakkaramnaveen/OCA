85e1859c99f3b5078a23a09fdfc5cb5a92ddd8f8
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-10 08:34:10 -0500

    4444444 4333334 4322234 4321234 4322234 4333334 4444444

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        StriverPatterns.print20(5);
+        StriverPatterns.print22(4);
     }
 }
--- src/main/java/org/example/StriverPatterns.java
+++ src/main/java/org/example/StriverPatterns.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import static java.lang.Math.min;
+
 public class StriverPatterns {
     static void print7(int n){
         for (int i = 0 ; i < n; i++ ) {
@@ -191,6 +193,30 @@ public class StriverPatterns {
             System.out.println();
             if (i < n) spaces -= 2;
             else spaces += 2;
+        }
+    }
+    static void print21(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i==0 || j==0 || j==n-1 || i ==n-1){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    static void print22(int n){
+        for (int i = 0; i < 2*n-1; i++) {
+            for (int j = 0; j < 2*n-1; j++) {
+                int top = i;
+                int left = j;
+                int right = (2*n-2)-j;
+                int bottom = (2*n-2)-i;
+                System.out.print(n-min(min(top, bottom),min(left,right)));
+            }
+            System.out.println();
         }
     }
 }
