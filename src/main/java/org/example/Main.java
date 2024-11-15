95c15978bda8fae57c4bcbab257497774865712c
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-15 08:58:53 -0500

    4444444 4333334 4322234 4321234 4322234 4333334 4444444

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        StriverPatterns.print22(4);
+        System.out.println(nani.class);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Patterns");
+        }
     }
 }
