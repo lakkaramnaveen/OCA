8f34a393f1b49b528c9ae94688bbaf93bdaec77e
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-17 14:05:03 -0500

    different number systems

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         byte sum = (byte) 1240;
         int total = 48764;
-        long population = 214748368;
+        long population = 214748L;
         System.out.println(((Object) population).getClass().getSimpleName());
+
+        int baseDecimal = 267;
+        int octVal = 0413;
+        int hexVal = 0x10B;
+        int binVal = 0b100001011;
 
     }
 }
