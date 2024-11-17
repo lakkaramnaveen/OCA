4fb1c53eedc6341a7200377ec8017eb1d337fdc2
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-17 13:39:32 -0500

    4444444 4333334 4322234 4321234 4322234 4333334 4444444

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,31 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(nani.class);
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Patterns");
-        }
+        //int x = 0;
+
+//        for(int i =0;i<x;i++){
+//            System.out.println(i);
+//            //10 times
+//        }
+//        //variable
+//
+//        while(x<5){
+//            System.out.println(x);
+//            x++;
+//        }
+
+        int x = 10;
+        double y = 20.5;
+
+        System.out.println(x.getClass().getSimpleName()); // Output: int
+        System.out.println(y.getClass().getSimpleName()); // Output: double
+
+        byte sum = (byte) 1240;
+        int total = 48764;
+        long population = 214748368;
+        System.out.println(population.getClass().getSimpleName());
+        do{
+            System.out.println(x);
+        }while(x++<5);
     }
 }
