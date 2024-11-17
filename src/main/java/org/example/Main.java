15e3c4c2a7f46376391a704ee6f110f33bc2d120
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-17 13:41:38 -0500

    primitive datatypes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,31 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //int x = 0;
-
-//        for(int i =0;i<x;i++){
-//            System.out.println(i);
-//            //10 times
-//        }
-//        //variable
-//
-//        while(x<5){
-//            System.out.println(x);
-//            x++;
-//        }
-
-        int x = 10;
-        double y = 20.5;
-
-        System.out.println(x.getClass().getSimpleName()); // Output: int
-        System.out.println(y.getClass().getSimpleName()); // Output: double
-
         byte sum = (byte) 1240;
         int total = 48764;
         long population = 214748368;
-        System.out.println(population.getClass().getSimpleName());
-        do{
-            System.out.println(x);
-        }while(x++<5);
+        System.out.println(((Object) population).getClass().getSimpleName());
+
     }
 }
