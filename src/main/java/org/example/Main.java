84f5ae0ce53e9f95940b91e3ee21c50f4c48d5de
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-18 18:22:52 -0500

    different number systems

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int baseDecimal = 267;
-        int octVal = 0413;
-        int hexVal = 0x10B;
-        int binVal = 0b100001011;
-        System.out.println (baseDecimal + octVal);
-        System.out.println (hexVal + binVal);
+        long var1 = 0_100_267_760;
+//        long var2 = 0_x_4_13;
+//        long var3 = 0b_x10_BA_75;
+        long var4 = 0b10000_10_11;
+//        long var5 = 0xa10_AG_75;
+        long var6 = 0x1_0000_10;
+        long var7 = 100__12_12;
+        long binVal = 0b1_0000_10_11;
     }
 }
