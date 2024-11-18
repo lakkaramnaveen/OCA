26e02e819bfc05f44c87df46f08e24af7d8e08fb
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-18 18:33:15 -0500

    different number systems

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 //        long var2 = 0_x_4_13;
 //        long var3 = 0b_x10_BA_75;
         long var4 = 0b10000_10_11;
-//        long var5 = 0xa10_AG_75;
+        long var5 = 0xa10_AD_10_F;
         long var6 = 0x1_0000_10;
         long var7 = 100__12_12;
         long binVal = 0b1_0000_10_11;
