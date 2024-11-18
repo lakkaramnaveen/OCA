0773dc3d239839ea97ecb2de1a9128732b062757
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-18 18:17:54 -0500

    different number systems

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        byte sum = (byte) 1240;
-        int total = 48764;
-        long population = 214748L;
-        System.out.println(((Object) population).getClass().getSimpleName());
-
-        long baseDecimal = 100_267_760;
-        long octVal = 04_13;
-        long hexVal = 0x10_BA_75;
-        long binVal = 0b1_0000_10_11;
+        int baseDecimal = 267;
+        int octVal = 0413;
+        int hexVal = 0x10B;
+        int binVal = 0b100001011;
+        System.out.println (baseDecimal + octVal);
+        System.out.println (hexVal + binVal);
     }
 }
