f776043c579da266397afbe9828ff616f5d02a10
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-22 14:31:33 -0500

    import default

--- src/main/java/org/example/ArgumentPassingExample.java
+++ src/main/java/org/example/ArgumentPassingExample.java
@@ -3,40 +3,6 @@ package org.example;
 public class ArgumentPassingExample {
 
     public static void main(String[] args) {
-        // Primitive type argument
-        int num = 10;
-        System.out.println("Original num: " + num);
-        modifyPrimitive(num);
-        System.out.println("After method call: " + num);
 
-        // Object reference argument
-        MyObject obj = new MyObject("Hello");
-        System.out.println("Original object: " + obj.getValue());
-        modifyObject(obj);
-        System.out.println("After method call: " + obj.getValue());
-    }
-
-    public static void modifyPrimitive(int value) {
-        value = 20;
-        System.out.println("Inside method: " + value);
-    }
-
-    public static void modifyObject(MyObject object) {
-        object.setValue("World");
-        System.out.println("Inside method: " + object.getValue());
-    }
-}
-
-class MyObject {
-    private String value;
-    // con
-    public MyObject(String value) {
-        this.value = value;
-    }
-    public String getValue() {
-        return value;
-    }
-    public void setValue(String value) {
-        this.value = value;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        long var1 = 0_100_267_760;
-//        long var2 = 0_x_4_13;
-//        long var3 = 0b_x10_BA_75;
-        long var4 = 0b10000_10_11;
-        long var5 = 0xa10_AD_10_F;
-        long var6 = 0x1_0000_10;
-        long var7 = 100__12_12;
-        long binVal = 0b1_0000_10_11;
 
-        float n = 1.213f;
-        double d = 1.12121d;
     }
 }
