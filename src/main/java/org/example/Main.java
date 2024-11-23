08e32b4cf3f8be78cfc0507b0ad4302eed385107
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-23 10:41:24 -0500

    operators

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Person p = new Person();
-        // can't access here
+        byte age1 = 10;
+        byte age2 = 20;
+        int age3 = 223;
+        long age4 = 1212122;
+        int s = age3+age2+age1;
+        // short s1_1 = (short) (age1+age2+age3+age4);
+        long s1 = age1+age2+age3+age4;
+        short sum = (short) (age1 + age2);
     }
 }
