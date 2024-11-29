d300dfbae4a8724d3da547830ad0d40af4d47af6
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-29 15:05:47 -0500

    operators

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        final byte age1 = 10;
-        final byte age2 = 20;
-        short sum = age1 + age2;
+
     }
 }
