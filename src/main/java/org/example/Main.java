9f1aaf30cd428e4115d870a6b6fc1141d8e1894a
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-11-29 15:06:25 -0500

    Error: Could not find or load main class org.example.ArgumentPassingExample Caused by: java.lang.ClassNotFoundException: org.example.ArgumentPassingExample

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        int i1 = 10;
+        int i2 = 20;
+        System.out.println(i1 >= i2);
+        long long1 = 10;
+        long long2 = 20;
+        System.out.println(long1 <= long2);
     }
 }
