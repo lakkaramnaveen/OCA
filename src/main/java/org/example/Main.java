eed361744f8a39ff2b659954959f67dfb3a953e0
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-02 12:24:03 -0500

    jdk 23 required -> if installed prints true compile time error for all other cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        var a = 5;
+        var b = 0.5;
+        var c = a + b;
+        System.out.println(c instanceof Double);
     }
 }
