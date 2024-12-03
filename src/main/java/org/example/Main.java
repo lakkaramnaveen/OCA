d17ec732dc88a09820b204e282d0cf08a9bb5191
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-03 10:06:14 -0500

    boolean

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        System.out.println(a == b);
-        System.out.println(a != b);
-        boolean b1 = false;
-        System.out.println(b1 == true);
-        System.out.println(b1 != true);
-        System.out.println(b1 == false);
-        System.out.println(b1 != false);
+
     }
 }
