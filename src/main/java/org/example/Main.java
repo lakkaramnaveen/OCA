27259f2d82c472041f216fcef2ebe0d514992a9f
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-04 12:01:25 -0500

    [1, 2, 3, 4]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>(List.of(1, 2, 3));
 
+        for (Integer i : list) {
+            if (i == 2) {
+                list.add(4);
+            }
+        }
+
+        System.out.println(list);
     }
 }
