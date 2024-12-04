9aceba3da6c4f5323dcf4c268a3e787332a461e5
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-04 12:13:07 -0500

    false true

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,10 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>(List.of(1, 2, 3));
+        String str1 = new String("Java");
+        String str2 = new String("Java");
 
-        for (Integer i : list) {
-            if (i == 2) {
-                list.add(4);
-            }
-        }
-
-        System.out.println(list);
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
     }
 }
