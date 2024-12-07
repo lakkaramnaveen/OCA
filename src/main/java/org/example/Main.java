6efc3d8fd0a44b8ef6704e2d8fe42020b81f8908
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-07 07:56:26 -0500

    false 8 true 8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,11 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        String str1 = new String("Java");
-        String str2 = new String("Java");
-
-        System.out.println(str1 == str2);
-        System.out.println(str1.equals(str2));
+        int marks = 8;
+        int total = 10;
+        System.out.println(total < marks && ++marks > 5);
+        System.out.println(marks);
+        System.out.println(total == 10 || ++marks > 10);
+        System.out.println(marks);
     }
 }
