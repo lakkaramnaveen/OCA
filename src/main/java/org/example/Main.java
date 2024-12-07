9ad302bbbabe394afdd858cd6241fccbcd181fbd
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-07 09:00:24 -0500

    300

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 40;
-        System.out.println(a++ > 10 || ++b < 30); // line1
-        System.out.println(a > 90 && ++b < 30);
-        System.out.println(!(c>20) && a==10 );
-        System.out.println(a >= 99 || a <= 33 && b == 10);
-        System.out.println(a >= 99 && a <= 33 || b == 10);
+        int int1 = 10, int2 = 20, int3 = 30;
+        System.out.println(int1 % int2 * int3 + int1 / int2);
     }
 }
