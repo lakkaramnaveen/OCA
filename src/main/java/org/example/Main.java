260b7c41355ccc4c9b63cccfaeae05e052565e22
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-09 15:06:05 -0500

    Integer Wrapper classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,11 @@
 package org.example;
 
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Boolean bool1 = true;
-        Character char1 = 'a';
-        Byte byte1 = 10;
-        Double double1 = 10.98;
+        Integer n = 21;
+//        Integer n1 = new Integer("21");
+        Integer n2 = Integer.valueOf(21);
+        System.out.printf(String.valueOf(n2));
 
-        Boolean bool4 = Boolean.valueOf(true);
-        Boolean bool5 = Boolean.valueOf(true);
-        Boolean bool6 = Boolean.valueOf("TrUE");
-        Double double4 = Double.valueOf(10);
     }
 }
