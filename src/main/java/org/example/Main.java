b3123e4aadac033767eb39e15b2364e5813b34dd
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-09 13:53:15 -0500

    Wrapper classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,16 +10,6 @@ public class Main {
         Byte byte1 = 10;
         Double double1 = 10.98;
 
-//        Boolean bool2 = new Boolean(true);
-//        Character char2 = new Character('a');
-//        Byte byte2 = new Byte((byte)10);
-//        Double double2 = new Double(10.98);
-//
-//        //Character char3 = new Character("a");
-//        Boolean bool3 = new Boolean(true);
-//        Byte byte3 = new Byte("10");
-//        Double double3 = new Double("10.98");
-
         Boolean bool4 = Boolean.valueOf(true);
         Boolean bool5 = Boolean.valueOf(true);
         Boolean bool6 = Boolean.valueOf("TrUE");
