1af1a1b134fad3d6c1ed1a60e2b910f9da46ccb1
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-11 14:19:52 -0500

    valueOf, heap memory

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Integer n = 21;
-//        Integer n1 = new Integer("21");
-        Integer n2 = Integer.valueOf(21);
-        System.out.printf(String.valueOf(n2));
+        Long var1 = Long.valueOf(123);
+        Long var2 = Long.valueOf("123");
+        System.out.println(var1 == var2);
 
+        Long var3 = Long.valueOf(223);
+        Long var4 = Long.valueOf(223);
+        System.out.println(var3 == var4);
     }
 }
