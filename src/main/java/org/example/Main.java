bb9bb2f503f5aafba9958fd22b3cd6d469664d57
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-12 09:31:30 -0500

    case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,26 +5,5 @@ public class Main {
         Long var1 = Long.valueOf(123);
         Long var2 = Long.valueOf("123");
         System.out.println(var1 == var2);
-
-        Long var3 = Long.valueOf(223);
-        Long var4 = Long.valueOf(223);
-        System.out.println(var3 == var4);
     }
 }
-
-
-// customer eligligit for additionaal banking product let's say a new credit card he want to applu
-//
-
-// Under what condition an account or user is eligible and ineligible for another product
-// What observations
-// recommencations
-
-
-// 1. if the account is not active and was acquired and had restriction "Not eligible"
-// 2. if the account is not active and was not acquired "Not eligible"
-// 3. if it is active and no restrictions and is not active "not eligible"
-
-
-// remove this piece of code
-// 
