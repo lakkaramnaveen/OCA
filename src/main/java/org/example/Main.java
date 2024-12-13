5241f50bb38452168b17d64b9add005bab154015
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2024-12-13 10:34:23 -0500

    case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Long var1 = Long.valueOf(123);
-        Long var2 = Long.valueOf("123");
-        System.out.println(var1 == var2);
     }
 }
